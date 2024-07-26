a969806d6338f9b4d876516155e5f9cdbad06920
Precious Ighereno <preciousighereno@gmail.com>
2024-07-26 13:12:46 +0100

    second commit with major changes for the server

--- src/index.js
+++ src/index.js
@@ -1,4 +1,10 @@
 import express from 'express';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 const app = express();
 const port = 3000;
@@ -6,6 +12,9 @@ const port = 3000;
 
 app.use(express.static('public'));
 
+app.get('*', (req, res) => {
+    res.redirect('/')
+});
 
 app.listen(port, () => {
     console.log(`Server is running at http://localhost:${port}`);
